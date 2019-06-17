ea403f2fe263d9a784d6c3611db6073ecd014ab1
ajshedivy <ajshedivy@wisc.edu>
2019-06-17 17:59:27 -0500

    Working env script

--- example/pl-example.sh
+++ example/pl-example.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env/bash 
-#activate pathlinker_env in order to run pathlinker
+
+#attepts to activate pathlinker_env in order to run pathlinker
+
+#if env not configurered, then pathlinker_env will be created 
 
 conda init bash
 
-#source ~/Anaconda3/etc/profile.d/conda.sh
 
 eval "$(conda shell.bash hook)"
 conda activate pathlinker_env
+
+if [ $? -ne 0 ]; then
+	echo creating pathlinker_env environment
+	conda env create -f environment.yml
+	conda activate pathlinker_env
+fi
 conda info --envs
+
+ 
 
 
