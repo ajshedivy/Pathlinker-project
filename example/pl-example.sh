11686565db6a46980b28af9485206c1207a9339e
ajshedivy <ajshedivy@wisc.edu>
2019-06-23 13:36:38 -0500

    pl-example script, output files

--- example/pl-example.sh
+++ example/pl-example.sh
@@ -1,12 +1,10 @@
-#!/usr/bin/env/bash 
+#!/usr/bin/env/bash
 
 #attempts to activate pathlinker_env in order to run pathlinker
 
-#if env not configured, then pathlinker_env will be created 
+#if env not configured, then pathlinker_env will be created
 
-conda init bash
-
-
+#conda init bash
 eval "$(conda shell.bash hook)"
 conda activate pathlinker_env
 
@@ -15,8 +13,9 @@ if [ $? -ne 0 ]; then
 	conda env create -f environment.yml
 	conda activate pathlinker_env
 fi
-conda info --envs
 
- 
+#run PathLinker
+python ../run.py --output='script_out' sample-in-net.txt sample-in-nodetypes.txt
 
-
+#run PathLinker with option write-paths
+python ../run.py --write-paths --output='script_out' sample-in-net.txt sample-in-nodetypes.txt
